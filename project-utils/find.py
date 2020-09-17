06a6a5cc81dcbe57b2ba3d21cd4393b5f982c32c
August Shi <awshi2@illinois.edu>
2020-09-17 10:45:21 -0500

    Including utility find script

---
+++ project-utils/find.py
@@ -0,0 +1,18 @@
+import fnmatch
+import os
+
+def find_file(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+def find_directory(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in dirs:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
